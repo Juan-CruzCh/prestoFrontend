4a8604a39deedd40e59da1cb65e0833deb37b834
Juan carlos chocllu <j.chocllu@opti-system.com>
2026-01-31 12:15:57 -0400

    cambios

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 
+import { useEffect } from 'react'
 import './App.css'
 import { AppRouter } from './core/router/AppRouter'
+import { useAuthStore } from './core/context/auth'
 function App() {
+  const autenticacion = useAuthStore((s)=> s.verificarAuth)
+  useEffect(()=>{
+    autenticacion()
+  },[])
   return (
     <AppRouter />
   )
---
+++ src/core/context/auth.tsx
@@ -0,0 +1,22 @@
+import { create } from "zustand";
+import { verificarLogin } from "../../module/autenticacion/service/autenticacionService";
+interface AuthStore {
+  user: any | null;
+  loading: boolean;
+  verificarAuth: () => Promise<void>;
+}
+export const useAuthStore  = create<AuthStore>((set)=>({
+    loading:false,
+    user:null,
+    verificarAuth : async ()=>{
+        try {
+            const response = await verificarLogin()
+            console.log(response);
+               set({ user: response, loading: false })
+        } catch (error) {
+            
+        }
+
+    }
+
+}))
